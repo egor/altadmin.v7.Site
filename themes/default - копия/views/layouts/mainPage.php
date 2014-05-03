7c27f1fb4d7ab603542f4a36682c4175448d35e3
Egor Rihnov <egor.developer@gmail.com>
2014-05-03 11:36:58 +0300

    FrontEnd. Смена темы

---
+++ themes/default - копия/views/layouts/mainPage.php
@@ -0,0 +1,148 @@
+<!DOCTYPE html>
+<html lang="en" style="-webkit-text-size-adjust: 100%; -ms-text-size-adjust: 100%;">
+<head>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=0" />
+	<title>Curve - Free CSS template by ChocoTemplates.com</title>
+	<link rel="shortcut icon" type="image/x-icon" href="css/images/favicon.ico" />
+	<link rel="stylesheet" href="<?php echo Yii::app()->theme->baseUrl; ?>/css/style.css" type="text/css" media="all" />
+	<link rel="stylesheet" href="<?php echo Yii::app()->theme->baseUrl; ?>/css/flexslider.css" type="text/css" media="all" />
+	<link href='http://fonts.googleapis.com/css?family=Ubuntu:400,500,700&subset=latin,cyrillic-ext' rel='stylesheet' type='text/css' />
+	<script src="<?php echo Yii::app()->theme->baseUrl; ?>/js/jquery-1.8.0.min.js" type="text/javascript"></script>
+	<!--[if lt IE 9]>
+		<script src="js/modernizr.custom.js"></script>
+	<![endif]-->
+	<script src="<?php echo Yii::app()->theme->baseUrl; ?>/js/jquery.flexslider-min.js" type="text/javascript"></script>
+	<script src="<?php echo Yii::app()->theme->baseUrl; ?>/js/functions.js" type="text/javascript"></script>
+</head>
+<body>
+	<!-- wraper -->
+	<div id="wrapper">
+		<!-- shell -->
+		<div class="shell">
+			<!-- container -->
+			<div class="container">
+				<!-- header -->
+				<header id="header">
+					<h1 id="logo"><a href="#">Curve</a></h1>
+					<!-- search -->
+					<div class="search">
+						<form method="post">
+							<span class="field"><input type="text" class="field" value="keywords here ..." title="keywords here ..." /></span>
+							<input type="submit" class="search-btn" value="" />
+						</form>
+					</div>
+					<!-- end of search -->
+				</header>
+				<!-- end of header -->
+				<?php $this->widget('application.widgets.Menu', array('method' => 'horizontalMenu')); ?>        
+				<!-- slider -->
+				<?php $this->widget('application.widgets.MainPage', array('method' => 'slider')); ?>		
+				<!-- end of slider -->
+				<!-- main -->
+                
+				<div class="main">
+					<a href="#" class="m-btn-grey grey-btn">request a demo</a>
+					<section class="cols">
+						<div class="col">
+							<img src="<?php echo Yii::app()->theme->baseUrl; ?>/images/col-img1.png" alt="" />
+							<div class="col-cnt">
+								<h2>Дизайн</h2>
+								<p>Творческий процесс создания вещи, в котором эстетика определяет суть</p>
+								<a href="#" class="more">подробнее</a>
+							</div>
+						</div>
+						<div class="col">
+							<img src="<?php echo Yii::app()->theme->baseUrl; ?>/images/col-img2.png" alt="" />
+							<div class="col-cnt">
+								<h2>Брендирование</h2>
+								<p>Бренд как информация, сохранённая в памяти потребителя</p>
+								<a href="#" class="more">подробнее</a>
+							</div>
+						</div>
+						<div class="col">
+							<img src="<?php echo Yii::app()->theme->baseUrl; ?>/images/col-img3.png" alt="" />
+							<div class="col-cnt">
+								<h2>Разработка</h2>
+								<p>Lorem ipsum dolor sit amet, consectetur adipiscing dolor</p>
+								<a href="#" class="more">подробнее</a>
+							</div>
+						</div>
+						<div class="cl">&nbsp;</div>
+					</section>
+					<section class="post">						
+                        <h1><?php echo $this->pageHeader; ?></h1>
+                        <br />
+                        <?php echo $content; ?>                        						
+						<div class="cl">&nbsp;</div>
+					</section>					
+				</div>
+				<!-- end of main -->
+				<div class="socials">
+					<div class="socials-inner">
+						<h3>Follow us</h3>
+						<ul>
+							<li><a href="#" class="facebook-ico"><span></span>Facebook</a></li>
+							<li><a href="#" class="twitter-ico"><span></span>Twitter</a></li>
+							<li><a href="#" class="rss-feed-ico"><span></span>Rss-feed</a></li>
+							<li><a href="#" class="myspace-ico"><span></span>myspace</a></li>
+							<li><a href="#" class="john-doe-123-ico"><span></span>Skype</a></li>
+						</ul>
+						<div class="cl">&nbsp;</div>
+					</div>
+				</div>
+				<div id="footer">
+					<div class="footer-cols">
+						<div class="col">
+							<h2>Services</h2>
+							<ul>
+								<li><a href="#">Web Design</a></li>
+								<li><a href="#">Branding</a></li>
+								<li><a href="#">Seo Optimization</a></li>
+								<li><a href="#">Mobile App Development</a></li>
+							</ul>
+						</div>
+						<div class="col">
+							<h2>Projects</h2>
+							<ul>
+								<li><a href="#">Lorem ipsum dolor </a></li>
+								<li><a href="#">Consectetuer adipiscing</a></li>
+								<li><a href="#">Proin sed odio et ante </a></li>
+								<li><a href="#">Mazim sensibus et usu</a></li>
+							</ul>
+						</div>
+						<div class="col">
+							<h2>Solutions</h2>
+							<ul>
+								<li><a href="#">Lorem ipsum dolor</a></li>
+								<li><a href="#">Consectetuer adipiscing</a></li>
+								<li><a href="#">Proin sed odio et ante </a></li>
+								<li><a href="#">Mazim sensibus et usu</a></li>
+							</ul>
+						</div>
+						<div class="col">
+							<h2>Partners</h2>
+							<ul>
+								<li><a href="#">Company Name 1</a></li>
+								<li><a href="#">Company Name 2</a></li>
+								<li><a href="#">Company Name 3</a></li>
+								<li><a href="#">Company Name 4</a></li>
+							</ul>
+						</div>
+						<div class="cl">&nbsp;</div>
+					</div>
+					<!-- end of footer-cols -->
+					<div class="footer-bottom">
+                        <?php $this->widget('application.widgets.Menu', array('method' => 'footerMenu')); ?>        						
+						<p class="copy">&copy; Copyright 2012 - <?php echo date('Y'); ?> ALT Site <span>|</span> <strong>Design by <a href="http://alt.dp.ua" target="_blank">alt.dp.ua</a></strong></p>
+						<div class="cl">&nbsp;</div>
+					</div>
+				</div>
+			</div>
+			<!-- end of container -->	
+		</div>
+		<!-- end of shell -->	
+	</div>
+	<!-- end of wrapper -->
+</body>
+</html>
--- themes/default/widgets/Menu/horizontalMenu.php
+++ themes/default/widgets/Menu/horizontalMenu.php
@@ -1,14 +1,28 @@
-<!-- navigation -->
-<nav id="navigation">
-    <a href="#" class="nav-btn">1HOME<span class="arr"></span></a>
-    <ul>
-        <?php
-        $path = explode('?', Yii::app()->request->getRequestUri());
-        $path = explode('/', $path[0]);
-        foreach ($menu as $value) {
-            echo '<li '.(in_array($value->url, $path) || $value->url == 'main' && $path[1]=='' ? 'class="active"' : '').'><a href="/' . ($value->url == 'main' ? '' : $value->url) . '">'.$value->menuName.'</a></li>';
-        }
-        ?>
-    </ul>
-</nav>
-<!-- end of navigation -->
+<div class="navbar-wrapper">
+    <div class="container">
+        <div class="navbar navbar-inverse navbar-static-top" role="navigation">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="/">Project name</a>
+                </div>
+                <div class="navbar-collapse collapse">
+                    <ul class="nav navbar-nav">
+                        <?php
+                        $path = explode('?', Yii::app()->request->getRequestUri());
+                        $path = explode('/', $path[0]);
+                        foreach ($menu as $value) {
+                            echo '<li ' . (in_array($value->url, $path) || $value->url == 'main' && $path[1] == '' ? 'class="active"' : '') . '><a href="/' . ($value->url == 'main' ? '' : $value->url) . '">' . $value->menuName . '</a></li>';
+                        }
+                        ?>                        
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
