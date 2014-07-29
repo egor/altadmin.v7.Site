3c7b10aa81b83ca5e47d18f4cca78441c02cb7a8
Egor Rihnov <egor.developer@gmail.com>
2014-07-29 18:31:19 +0300

    Комментарии в новостях

--- themes/default/views/news/default/detail.php
+++ themes/default/views/news/default/detail.php
@@ -22,3 +22,13 @@ $this->widget('application.widgets.AdminBtn',
 <span class="label label-primary"><?php echo date('d.m.Y', $model->date); ?></span> <span class="label label-default"><?php echo $model->newsSection->name; ?></span>
 <?php
 echo $model->text;
+if (FrontEditFields::getSettings('CommentSettings', 'workNews') && Yii::app()->params['altadmin']['modules']['comment']['work'] && Yii::app()->params['altadmin']['modules']['news']['comment']) {
+    ?>
+    <div id="commentList">    
+    <?php    
+    $this->widget('application.widgets.SComment', array('method' => 'printComment', 'data' => array('type' => 'news', 'recordId' => $model->id)));
+    ?>
+    </div>
+    <?php    
+    $this->widget('application.widgets.SComment', array('method' => 'printForm', 'data' => array('pid' => 0, 'type' => 'news', 'recordId' => $model->id)));
+}
